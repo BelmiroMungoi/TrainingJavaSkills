b593f75f0fe27bf7544118f12927f0dd63b93b2f
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-04-14 23:38:34 +0200

    adicionando um doGet no controller

--- src/java/controller/LoginUser.java
+++ src/java/controller/LoginUser.java
@@ -18,26 +18,38 @@ import javax.servlet.http.HttpSession;
 public class LoginUser extends HttpServlet {
 
     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String log = request.getParameter("log");
+        if (log.equals("true")) {
+            HttpServletRequest servletRequest = (HttpServletRequest) request;
+            HttpSession session = servletRequest.getSession();
+            session.invalidate();
+            response.sendRedirect("login.jsp");
+        }
+    }
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         String userName = request.getParameter("nome");
         String password = request.getParameter("pass");
-
+        String url = request.getParameter("url");
         if (userName.equals("admin") && password.equals("admin")) {
 
             UserBeans beans = new UserBeans();
             beans.setUserName(userName);
             beans.setPassword(password);
-            String url = request.getParameter("url");
+
             HttpServletRequest servletRequest = (HttpServletRequest) request;
             HttpSession session = servletRequest.getSession();
             session.setAttribute("user", beans);
 
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/index.jsp");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/index.jsp");
             dispatcher.forward(request, response);
-    } else {
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+        } else {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
             dispatcher.forward(request, response);
         }
     }
