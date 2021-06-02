1edc408544aa086e6a48f481dff967986ab90fcd
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-06-02 09:10:59 +0200

    Controller para o upload de ficheiros

---
+++ src/java/controller/FileUpload.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author bbm29
+ */
+@WebServlet("/jsp/fileUpload")
+public class FileUpload extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String fileUpload = request.getParameter("fileUpload");
+        response.getWriter().write(" Upload Realizado com Sucesso!!!");
+    }
+
+}
