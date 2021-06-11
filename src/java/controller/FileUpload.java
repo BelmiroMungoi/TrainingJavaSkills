09b333ac516b42f9413031f76987e76d61843535
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-06-11 15:59:48 +0200

    adicionado novos campos e salvado a imagem

--- src/java/controller/FileUpload.java
+++ src/java/controller/FileUpload.java
@@ -6,6 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.beans.UserBeans;
+import model.dao.DaoUsuario;
 
 /**
  *
@@ -13,6 +15,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/jsp/fileUpload")
 public class FileUpload extends HttpServlet {
+
+    private DaoUsuario usuario = new DaoUsuario();
+    private UserBeans beans = new UserBeans();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -23,8 +28,24 @@ public class FileUpload extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String fileUpload = request.getParameter("fileUpload");
-        response.getWriter().write(" Upload Realizado com Sucesso!!!");
+        try {
+            String username = request.getParameter("user");
+            String password = request.getParameter("pass");
+            String fileUpload = request.getParameter("fileUpload");
+
+            beans.setUserName(username);
+            beans.setPassword(password);
+            beans.setImage(fileUpload);
+            usuario.salvarUsuario(beans);
+
+            response.setStatus(200);
+            response.getWriter().write(" Upload Realizado com Sucesso!!!");
+
+        } catch (Exception e) {
+            response.getWriter().write("Erro ao fazer o upload" + e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 
 }
