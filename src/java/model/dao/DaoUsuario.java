ae78a70a2e823d59572057d430157ce6befa6d1c
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-06-15 22:18:43 +0200

    adiconado um metodo para listagem e outro para buscar a imagem

--- src/java/model/dao/DaoUsuario.java
+++ src/java/model/dao/DaoUsuario.java
@@ -2,7 +2,13 @@ package model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import model.beans.UserBeans;
 import model.connection.SingleConnection;
 
@@ -36,6 +42,40 @@ public class DaoUsuario {
             }
         }
 
+    }
+
+    public List<UserBeans> listarUsuario() {
+        List<UserBeans> lista = new ArrayList<>();
+        try {
+            String sql = "select * from usuario";
+            PreparedStatement pst = connection.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                UserBeans beans = new UserBeans();
+                beans.setIdUser(rs.getInt("idUser"));
+                beans.setUserName(rs.getString("username"));
+                beans.setPassword(rs.getString("password"));
+                beans.setImage(rs.getString("imagem"));
+                lista.add(beans);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Oops!\nErro ao listar usuario");
+        }
+        return lista;
+    }
+
+    public String buscaImagem(String idUser) {
+        String sql = "select imagem from usuario where idUser =" + idUser;
+        try {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                return rs.getString("imagem");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoUsuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
 }
