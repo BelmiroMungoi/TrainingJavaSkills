678c49dd1acaea230ae4318c6112ec339ed62c41
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-06-15 22:19:07 +0200

    corrigido o codigo em ajax

--- web/js/imageUpload.js
+++ web/js/imageUpload.js
@@ -4,8 +4,6 @@ function uploadImage() {
     var target = document.querySelector("img");
     var file = document.querySelector("input[type=file]").files[0];
     var reader = new FileReader();
-    var username = $("#usernamee").val();
-    var password = $("#passwordd").val();
     reader.onloadend = function () {
         target.src = reader.result;
 
@@ -13,20 +11,17 @@ function uploadImage() {
         $.ajax({
             method: "POST",
             url: "fileUpload",
-            data: {fileUpload: target.src},
-            data: {user: username},
-            data: {pass: password}
-        })
-                .done(function (response) {
-                    alert(" " + response);
+            data: {fileUpload: target.src}
+        });
+        done(function (response) {
+            alert(" " + response);
 
-                })
-                .fail(function (xhr, status, errorThrown) {
-                    alert("Erro: " + xhr.responseText);
+        });
+        fail(function (xhr, status, errorThrown) {
+            alert("Erro: " + xhr.responseText);
 
-                });
+        });
     };
-
     if (file) {
         reader.readAsDataURL(file);
     } else {
