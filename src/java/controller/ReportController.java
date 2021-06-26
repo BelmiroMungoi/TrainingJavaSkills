c829e524eff2de28c82ed439d22fa383a54479ff
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-06-26 09:24:05 +0200

    codigo para buscar e baixar o relatorio

--- src/java/controller/ReportController.java
+++ src/java/controller/ReportController.java
@@ -1,86 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.beans.UserBeans;
+import model.dao.DaoUsuario;
+import services.ReportService;
 
 /**
  *
  * @author bbm29
  */
+@WebServlet("/jsp/reports")
 public class ReportController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ReportController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ReportController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+    private ReportService reportService = new ReportService();
+    private DaoUsuario daoUsuario = new DaoUsuario();
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            String reportType = request.getParameter("reportType");
+            List<UserBeans> usuarios = daoUsuario.listarUsuario();
+            List dados = new ArrayList();
+            dados.add(usuarios);
+            String reportUrl = reportService.generateReport(dados, new HashMap(),
+                    "rel_usuario ", "rel_usuario", request.getServletContext());
+
+            //Constroi o caminho do arquivo
+            File downloadFile = new File(reportUrl);
+            FileInputStream inputStream = new FileInputStream(downloadFile);
+
+            //Obtem o tipo MIME do arquivo
+            String mimeType = request.getServletContext().getMimeType(reportUrl);
+            if (mimeType == null) {
+                //definir um tipo binário se o mapeamento mime nao for encontrado
+                mimeType = "application/octet-stream";
+
+                //definir os atributos da resposta
+                response.setContentType(mimeType);
+                response.setContentLength((int) downloadFile.length());
+
+                // define o cabecalho da resposta
+                String headerKey = "Content-Disposition";
+                String headerValue = String.format("attachment; filename=\"%s\"",
+                        downloadFile.getName());
+
+                // obtem o fluxo de saida da resposta
+                OutputStream outputStream = response.getOutputStream();
+                byte[] buffer = new byte[4096];
+                int byteReader = -1;
+
+                //Escrever os bytes lidos apartir do fluxo de entrada para o fluxo de saida
+                while ((byteReader = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, byteReader);
+                }
+                inputStream.close();
+                outputStream.close();
+
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(ReportController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
