df3cf7431c0af0b01abaee2a2e6a7f4c074d2681
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-08-01 14:44:00 +0200

    getters e setters

---
+++ src/java/model/beans/Eventos.java
@@ -0,0 +1,46 @@
+package model.beans;
+
+/**
+ *
+ * @author bbm29
+ */
+public class Eventos {
+
+    private Long id;
+    private String descricao;
+    private String inicio;
+    private String fim;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getFim() {
+        return fim;
+    }
+
+    public void setFim(String fim) {
+        this.fim = fim;
+    }
+
+}
